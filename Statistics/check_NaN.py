404a46a1d645d4087eb9091be20eeb34e1a90583
csmaterial <csmaterial@users.noreply.github.com>
2018-01-06 13:47:27 +0800

    Create check_NaN.py

---
+++ Statistics/check_NaN.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 12:22:05 2018
+
+@author: zhang
+"""
+
+import numpy as np
+import pandas as pd
+import math
+
+filename = 'C:\\Users\\zhang\\Desktop\\d.csv'
+filename1 = 'C:\\Users\\zhang\\Desktop\\d_test.csv'
+stock = pd.read_csv(filename)
+stock1 = pd.read_csv(filename1)
+
+#check all column
+#l have the col which is none nan
+def checkCol(stock):
+    l = []
+    for num in range(stock.shape[1]):
+        count = 0
+        for i in range(stock.shape[0]):
+            #print(num,i)
+            if math.isnan(stock[str(num)][i]):
+                count+=1
+        if count == 0:
+            l.append(num)
+    return l
+l = checkCol(stock1)
+
+
